3a557efcb97670c2ec3d142992e7c278e323bdf6
Guglielmo Turco <guglielmo.turco@gmail.com>
2016-12-09 14:35:23 +0100

    Implemented non-Google API, clean the code and add comments to code

--- js/app.js
+++ js/app.js
@@ -9,10 +9,10 @@ function animateShow(el){
   el.animate({
     "left" : "0px"
   });
-};
+}
 $(".searchMenu").click(function(){
   var sideResults = $(".sideResults");
-    if(sideResults.position().left == 0)
+    if(sideResults.position().left === 0)
       animateHide(sideResults);
     else
       animateShow(sideResults);
@@ -41,6 +41,7 @@ var map;
 var markers = [];
 var locations = [];
 // load external data
+// The structure of the file loaded is inside the README
 $.getJSON( "js/POI.json", function( data ) {
     locations = data.locations;
     console.log("Load correctly data into app");
@@ -55,6 +56,7 @@ function initMap() {
     center: {"lat": 32.715738000	, "lng": -117.161083800	},
     zoom: 7,
     mapTypeControl: true,
+    // move the controller at the right side of the window
     mapTypeControlOptions: {
         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
         position: google.maps.ControlPosition.TOP_RIGHT
@@ -62,7 +64,7 @@ function initMap() {
   });
 
   var largeInfowindow = new google.maps.InfoWindow();
-
+// load all the locations inside markers and POIlist
   for (var i = 0; i < locations.length; i++) {
 
     var position = locations[i].coordinates;
@@ -79,6 +81,7 @@ function initMap() {
     });
     // Push the marker to our array of markers.
     markers.push(marker);
+    // POIlist is used for the side menu. I implemented the favorite with the use of an observable, so I could change the class of the items dynamically
     POIlist.push({name: title, id: i, category : category, favorite: ko.observable(favorite)});
     // Create an onclick event to open the large infowindow at each marker.
     marker.addListener('click', function() {
@@ -90,12 +93,11 @@ function initMap() {
 
   var bounds = new google.maps.LatLngBounds();
   // Extend the boundaries of the map for each marker and display the marker
-  for (var i = 0; i < markers.length; i++) {
+  for (i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
     bounds.extend(markers[i].position);
   }
   map.fitBounds(bounds);
-
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
@@ -106,12 +108,12 @@ function populateInfoWindow(marker, infowindow) {
   if (infowindow.marker != marker) {
     // Clear the infowindow content to give the streetview time to load.
     infowindow.setContent('');
-    var innerHTML = '';
     infowindow.marker = marker;
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
       infowindow.marker = null;
     });
+
     var streetViewService = new google.maps.StreetViewService();
     var radius = 50;
     function getStreetView(data, status) {
@@ -119,7 +121,7 @@ function populateInfoWindow(marker, infowindow) {
         var nearStreetViewLocation = data.location.latLng;
         var heading = google.maps.geometry.spherical.computeHeading(
           nearStreetViewLocation, marker.position);
-          infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+          infowindow.setContent('<h4 class="markerName">' + marker.title + '</h4><div id="pano"></div><ul id="news">'+news+'</ul>');
           var panoramaOptions = {
             position: nearStreetViewLocation,
             pov: {
@@ -130,10 +132,32 @@ function populateInfoWindow(marker, infowindow) {
         var panorama = new google.maps.StreetViewPanorama(
           document.getElementById('pano'), panoramaOptions);
       } else {
-        infowindow.setContent('<div>' + marker.title + '</div>' +
-          '<div>No Street View Found</div>');
+        infowindow.setContent('<h4 class="markerName">' + marker.title + '</h4>' + '<div>No Street View Found</div><ul id="news">'+news+'</ul>');
       }
     }
+    // load the news from the New York Times inside the infowindow
+    // this code was taken from the New York Times API console.
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+      'api-key': "117512e0e9bc4b1dafd16fe2ec69a9c3",
+      'q': marker.title,
+      'fl': "headline,web_url",
+      'sort': "newest"
+    });
+    var news = '';
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(data) {
+      $.each( data.response.docs, function( i, item ) {
+        news += '<li class="newsHeadline"><a class="newsLink" href="' + item.web_url +'">'+item.headline.main+'</a></li>';
+      });
+      $("#news").append(news);
+    }).fail(function(err) {
+      news += '<li>No news</li>';
+      $("#news").append(news);
+      console.log( err);
+    });
     // Use streetview service to get the closest streetview image within
     // 50 meters of the markers position
     streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
@@ -182,7 +206,10 @@ ko.applyBindings(new locationsViewModel());
 // trigger the click event on the specific list item, to show the corresponding marker on the map
 function openInfoWindow(){
   google.maps.event.trigger(markers[this.id], 'click');
-};
+}
+
+// change icon of pin when mouseover is triggered
+
 function changePinColor(){
   var self = this;
   if(self.favorite())
@@ -190,6 +217,9 @@ function changePinColor(){
   else
     markers[this.id].setIcon(icons.ylw_pin);
 }
+
+// change icon back to deafult
+
 function defaultPinColor(){
   var self = this;
   if(self.favorite())
@@ -197,19 +227,19 @@ function defaultPinColor(){
   else
     markers[self.id].setIcon(icons.red_pin);
 }
+
+// switch between favorite true/false when the span is clicked
+
 function favoriteLocation(){
   var self = this;
-
-  if(self.favorite()){
+  if(self.favorite())
     self.favorite(false);
-
-  }
-  else{
+  else
     self.favorite(true);
-
-  }
 }
+
+// change class based on the favorite value
 
 function isFavorite(el){
   return el.favorite() ? "icon-star-full" : "icon-star-empty iconHidden";
-};
+}
